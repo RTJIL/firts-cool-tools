9fbc3ae71b439625064900b7c90c36d4c9a6524b
Artem <161304626+RTJIL@users.noreply.github.com>
2025-06-15 11:13:05 +0000

    more pracs with state

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Appp from './List.jsx' 
-import Apppp from './Proptst.jsx'
-import MailClient from './StateStructure.jsx'
+import Person from './setStatePrac'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MailClient />
+    <Person />
   </StrictMode>,
 )
--- src/setStatePrac.jsx
+++ src/setStatePrac.jsx
@@ -1,38 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function Person() {
-  const [person, setPerson] = useState({ name: "John", age: 100 });
+export default function SyncedInputs() {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const fullName = [name, surname].filter(Boolean).join(' ');
 
-  const handleIncreaseAge = () => {
-    console.log("in handleIncreaseAge (before setPerson call): ", person);
-    setPerson({ ...person, age: person.age + 1 });
-    // we've called setPerson, surely person has updated?
-    console.log("in handleIncreaseAge (after setPerson call): ", person);
-  };
-
-  /* const handleIncreaseAgeT = () => {
-  setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-  setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
-  console.log('What', person.age); // 🔥 This will still show the old value
-}; */
-
-  // this console.log runs every time the component renders
-  // what do you think this will print?
-  console.log("during render: ", person);
+  function handleInputChange(setter) {
+  return (e) => setter(e.target.value);
+}
 
   return (
     <>
-      <h1>{person.name}</h1>
-      <h2>{person.age}</h2>
-      <button onClick={handleIncreaseAge}>Increase age</button>
+      <Input label="Name" text={name} onType={handleInputChange(setName)} placeholder="Enter name"/>
+      <Input label="Surname" text={surname} onType={handleInputChange(setSurname)} placeholder="Enter name"/>
+      <h1>FullName: {fullName}</h1>
     </>
   );
 }
 
-function Component() {
-  const [count, setCount] = useState(0);
-
-  setCount(count + 1); // 🚨 This is the issue
-
-  return <h1>{count}</h1>;
+function Input({ label, text, onType, placeholder }) {
+  return (
+    <label>
+      {label} <input value={text} onChange={onType} placeholder={placeholder} />
+    </label>
+  );
 }
